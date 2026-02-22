4be8609c9f9141973477677c00253609dbc1bcec
Jhonatas Magalhães <jhonatasbarbosa@yahoo.com.br>
2026-02-22 11:45:45 -0300

    chore: Remove unused classes in dark mode and create a method for the exit button.

--- src/app/presentation/layouts/drawer/drawer-footer/drawer-footer.component.ts
+++ src/app/presentation/layouts/drawer/drawer-footer/drawer-footer.component.ts
@@ -1,6 +1,6 @@
-import { Component } from "@angular/core";
+import { Component, inject } from "@angular/core";
 import { MatIcon } from "@angular/material/icon";
-import { RouterModule } from "@angular/router";
+import { Router, RouterModule } from "@angular/router";
 
 @Component({
   selector: "app-drawer-footer",
@@ -8,4 +8,9 @@ import { RouterModule } from "@angular/router";
   templateUrl: "./drawer-footer.component.html",
   styleUrl: "./drawer-footer.component.scss"
 })
-export class DrawerFooterComponent {}
+export class DrawerFooterComponent {
+  private router = inject(Router);
+  logout() {
+    this.router.navigate(["/login"]);
+  }
+}
