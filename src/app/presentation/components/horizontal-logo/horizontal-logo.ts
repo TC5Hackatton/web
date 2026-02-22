cfd835d74d0188713364d11ddc5e3a5ee0d9cd13
Jhonatas Magalhães <jhonatasbarbosa@yahoo.com.br>
2026-02-22 12:08:35 -0300

    chore: It adds a logo for dark mode and comments out sections that won't be used in the settings screen.

--- src/app/presentation/components/horizontal-logo/horizontal-logo.ts
+++ src/app/presentation/components/horizontal-logo/horizontal-logo.ts
@@ -1,4 +1,5 @@
-import { Component, Input } from "@angular/core";
+import { Component, Input, inject, computed } from "@angular/core";
+import { AppSettingsService } from "../../services/app-settings.service";
 
 @Component({
   selector: "app-horizontal-logo",
@@ -7,4 +8,11 @@ import { Component, Input } from "@angular/core";
 })
 export class HorizontalLogoComponent {
   @Input() width = "auto";
+
+  private settingsService = inject(AppSettingsService);
+
+  logoSrc = computed(() => {
+    const isDark = this.settingsService.settings().appearance.dark_mode;
+    return isDark ? "MindEase_horizontal_modoEscuro.svg" : "MindEase_horizontal.svg";
+  });
 }
